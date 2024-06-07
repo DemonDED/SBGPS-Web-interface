6eb0ed3701377e603836a8626778c342931b6cb0
Aleksandrov Dmitriy <DemonDED@inbox.ru>
2024-06-07 09:32:22 +0700

    Общий компонент Footer готов

---
+++ src/components/Layout/FooterElem.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <div class="footer">
+    <picture>
+      <source srcset="@/assets/footer/footer-768.webp" media="(min-width: 768px)" width="153" height="65">
+      <img decoding="async" src="@/assets/footer/footer-320.webp" alt="НПФ 'Гранч'" width="108" height="44">
+    </picture>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .footer {
+    flex: 0 0 auto;
+    background: $gray;
+  }
+</style>
