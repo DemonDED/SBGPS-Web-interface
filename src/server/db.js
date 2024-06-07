7acc563913a3a123a6033f4cd3420a35bc3ce0e6
Aleksandrov Dmitriy <DemonDED@inbox.ru>
2024-06-07 09:37:09 +0700

    Локальное хранилище данных

---
+++ src/server/db.js
@@ -0,0 +1,75 @@
+export const monitoring_data = {
+  basic: {
+    serialnum: '6908 1117 0108',
+    fwversion_router: '3.5.9',
+    load_average: ['0.01', '0.13', '0.12'],
+    ip: '10.87.255.10',
+    ap_linking: 5,
+    adhoc_linking: 3,
+  },
+  power: {
+    currtime: 58822766,
+    uout: 5369,
+    iout: 854,
+    uacc: 3482,
+    iacc: 0,
+    uf1: 59505,
+    if1: 131,
+    uf2: 0,
+    if2: 0,
+    protocolver: '0005',
+    charge: 100,
+    accumact: 0,
+    positioning_module: true,
+  }
+}
+export const monitoring_lan = [
+  {
+    id: 1,
+    name_port: '',
+    mac_addr: '',
+    ip_addr: '',
+    tx: Math.random(20000),
+    rx: Math.random(20000),
+    link: true,
+  },
+  {
+    id: 2,
+    name_port: '',
+    mac_addr: '',
+    ip_addr: '',
+    tx: Math.random(20000),
+    rx: Math.random(20000),
+    link: true,
+  },
+  {
+    id: 3,
+    name_port: '',
+    mac_addr: '',
+    ip_addr: '',
+    tx: Math.random(20000),
+    rx: Math.random(20000),
+    link: true,
+  },
+]
+
+export const monitoring_wlan = [
+  {
+    id: 1,
+    name_port: '',
+    mac_addr: '',
+    ip_addr: '',
+    tx: Math.random(20000),
+    rx: Math.random(20000),
+    link: true,
+  },
+  {
+    id: 2,
+    name_port: '',
+    mac_addr: '',
+    ip_addr: '',
+    tx: Math.random(20000),
+    rx: Math.random(20000),
+    link: true,
+  },
+]
