7df17177f736e02e276797ec60970634398054ff
Aleksandrov Dmitriy <DemonDED@inbox.ru>
2024-06-13 00:47:20 +0700

    Правки стилей для страницы настроек

--- src/views/SettingsView.vue
+++ src/views/SettingsView.vue
@@ -91,9 +91,9 @@ axios.get('http://localhost:3000/monitoring/lan')
 </script>
 
 <template>
-  <h1>Settings</h1>
-
+  
   <div class="settings">
+    <h1>Settings</h1>
     <div class="settings__lan">
       <div class="settings__lan__item" v-for="item in settings_lan" :key="item.id">
         <label class="port-ip" :for="item.id">{{ item.name_port }}</label>
@@ -101,7 +101,7 @@ axios.get('http://localhost:3000/monitoring/lan')
       </div>
     </div>
 
-    <div class="settings_wlan">
+    <div class="settings__wlan">
       <div class="settings__wlan__item" v-for="item in settings_wlan" :key="item.id">
         <label class="port-ip-wlan" :for="item.id">{{ item.name_port }}</label>
         <input class="ip-addr-wlan" type="text" :id="item.id" v-model="item.ip_addr">
@@ -113,5 +113,63 @@ axios.get('http://localhost:3000/monitoring/lan')
 </template>
 
 <style lang="scss" scoped>
-  
+    .settings {
+    padding: 2rem 1.6rem;
+    color: $orange;
+    @include flex(column);
+    @include flex-utils(flex-start, center);
+    gap: 3.5rem;
+    flex: 1 0 auto;
+    background: $gray;
+    box-sizing: border-box;
+
+    &__main {
+      padding: 0 1.6rem;
+      width: 100%;
+      max-width: 72.8rem;
+      @include flex(column);
+      text-align: left;
+      box-shadow: 0 1rem 0 0 $dark-gray;
+      border-radius: 1.6rem;
+
+      &__title {
+        background: $dark-gray;
+        border-radius: 1.6rem 1.6rem 0 0;
+      }
+    }
+
+    &__lan, &__wlan {
+      @include flex(column);
+      @include flex-utils(flex-start, center);
+      gap: 2rem;
+
+      &__item {
+        @include flex();
+        @include flex-utils(flex-start, center);
+        gap: 1rem;
+
+        input {
+          background-color: $off-gray;
+          border: 1px solid #676767;
+          border-radius: 1.6rem;
+          padding: 1rem;
+          color: $orange;
+        }
+      }
+    }
+
+  }
+
+  button {
+    cursor: pointer;
+    padding: 1.6rem 2rem;
+    text-decoration: none;
+    color: $orange;
+    background-color: $off-gray;
+    border-radius: 1.6rem;
+    box-sizing: border-box;
+    width: 100%;
+    max-width: 23rem;
+    border: none;
+  }
 </style>
